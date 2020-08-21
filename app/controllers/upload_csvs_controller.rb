3b3a31f150d31592677765d40baec9e747a02fa0
Mohammad Shahnawaz <shahnawazkits@gmail.com>
2020-08-21 21:52:02 +0530

    Updated action mailer

--- app/controllers/upload_csvs_controller.rb
+++ app/controllers/upload_csvs_controller.rb
@@ -53,23 +53,21 @@ class UploadCsvsController < ApplicationController
     link_array_group = link_array.group_by(&:itself).map { |k,v| [k, v.length] }.to_h
     @new_array.push([row_map[:refferal_link],row_map[:home_link],"#{link_array_group[row_map[:home_link]]}"])  
   end
-      file = Tempfile.open(["#{Rails.root}/public/generated_csv", ".csv"]) do |csv|
-
-       csv << ["referal link", "home_link", "count"]
-       @new_array.each do |new_array|
-          csv << new_array
-      end
-      file = "#{Rails.root}/public/product_data.csv"
-      headers = ["referal link", "home_link", "count"]
-      file = CSV.open(file, 'w', write_headers: true, headers: headers) do |writer|
-
-         @new_array.each do |new_array|
-          writer << new_array
-      end
-      
-      upload_csv.update(:generated_csv => file)
-      end
+    file = Tempfile.open(["#{Rails.root}/public/generated_csv", ".csv"]) do |csv|
+     csv << ["referal_link", "home_link", "count"]
+     @new_array.each do |new_array|
+        csv << new_array
     end
+    file = "#{Rails.root}/public/product_data.csv"
+    headers = ["referal link", "home_link", "count"]
+    file = CSV.open(file, 'w', write_headers: true, headers: headers) do |writer|
+       @new_array.each do |new_array|
+        writer << new_array
+    end
+    
+    upload_csv.update(:generated_csv => file)
+    end
+  end
     UserMailer.send_csv(upload_csv).deliver_now!
 end
   # PATCH/PUT /upload_csvs/1
