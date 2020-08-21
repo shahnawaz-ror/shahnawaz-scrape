227b10bfd83a4cd5a2c37aee9989d83c8c9fad7b
Mohammad Shahnawaz <shahnawazkits@gmail.com>
2020-08-21 21:12:33 +0530

    Intial app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://fathomless-castle-93342.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: 'roneysmith34@gmail.com',
-    password: 'roney123',
+    user_name: 'railschecker@gmail.com',
+    password: 'Shahnawaz786@!g',
     domain: 'gmail.com',
     address: 'smtp.gmail.com',
     port: '587',
