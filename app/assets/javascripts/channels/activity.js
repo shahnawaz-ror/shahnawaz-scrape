f6878c292bab2f73b40425b13f79861a5224c6aa
Mohammad Shahnawaz <shahnawazkits@gmail.com>
2020-08-22 11:55:49 +0530

    Updated js

--- app/assets/javascripts/channels/activity.js
+++ app/assets/javascripts/channels/activity.js
@@ -2,12 +2,14 @@ App.conversation = App.cable.subscriptions.create("ActivityChannel", {
   connected: function() {},
   disconnected: function() {},
   received: function(data) {
-    alert(data);
+    swal("Congratulations new CSV parsed!!!", data["message"])
+
+    // alert(data["message"]);
   },
   speak: function(message) {
     // return this.perform('speak', {
     //   message: message
     // });
-    alert(message);
+    // alert(data["message"]);
   }
 });
